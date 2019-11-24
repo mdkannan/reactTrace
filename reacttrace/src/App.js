d516ebcfcfe39fcc1a3eb5222c5f6d7d804c7e31
mdkannan <mdkannandurai88@gmail.com>
2019-11-25 03:42:21 +0530

    third commit

--- reacttrace/src/App.js
+++ reacttrace/src/App.js
@@ -3,6 +3,14 @@ import { Component } from "react";
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+import DateTimePicker from 'react-datetime-picker';
+import Popup from "reactjs-popup";
+
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+
+import 'react-datepicker/dist/react-datepicker.css';
+
 import './App.css';
 
 //function App() {
@@ -10,7 +18,7 @@ class App extends Component {
 
     constructor(props) {
         super(props);
-
+        let date = `2015-06-26`;
         this.state = {
             columnDefs: [
                 {headerName: "Date", field: "time"},
@@ -23,50 +31,72 @@ class App extends Component {
             ],
             paginationPageSize:50,
             rowData: [],
-            allLog:[]
+            allLog:[],
+            startDate: moment(),
+            endDate:moment(),
+            date: new Date(),
+            endDatePic:new Date()
         }
 
         this.onFind = this.onFind.bind(this)
+      //  this.onChange = this.onChange.bind(this)
+     //   this.handleChange = this.handleChange.bind(this);
+    //    this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    onChange = date => this.setState({ date })
+    onChangeEd = endDatePic => this.setState({ endDatePic })
+    handleChange(date) {
+        this.setState({
+            startDate: date
+        })
+        this.setState({
+            endDate: date
+        })
+    }
+
+
     onFind(){
-        console.log(this.refs.endtime.value);
+        let url, val,mainUrl;
+        // console.log(this.refs.starttime.value);
+        let st = this.state.date;  //2019-11-24T19:09:57.415Z
+        let n = st.toISOString(); //2015-08-28T10:00:00Z //2015-08-28T10:00:00.000Z
+
+        let et = this.state.endDatePic;
+        let endtime = et.toISOString();
         this.setState({paginationPageSie:this.refs.pagezination.value});
-        //http://localhost:12345/log?startTime=2015-08-28T10:
-        // 00:00Z&endTime=2015-08-28T11:00:00Z&cat=OSPF&prio=3&num=40
-        let url="http://localhost:4001/log";
+        if(this.refs.logserver.value == '4001' || this.refs.logserver.value == '4002' || this.refs.logserver.value == '4003'){
+            if(this.refs.logserver.value !== '' && this.refs.cat.value != '' && this.refs.severity.value != '') {
+                if(this.refs.cat.value != 'clog' && this.refs.severity.value != 'ser'){
+                    url = "http://localhost:" + this.refs.logserver.value + "/log";
+                    val = "&cat=" + this.refs.cat.value + "&prio=" + this.refs.severity.value + "&num=20";
+                    mainUrl = url + "?startTime=" + n + "&endTime=" + endtime + val;
+                }
+            }
+        }
         let url2 ="http://localhost:4002/log";
         let url3 ="http://localhost:4003/log";
-        let val ="&cat="+this.refs.cat.value+"&prio="+this.refs.severity.value+"&num=20";
-        //2015-08-28T10:59:53Z
         let fullUrl=url+val;
-       // fetch(url+"?startTime=2015-08-28T10:\n" +
-       //     "00:00Z&endTime=2015-08-28T11:00:00Z&cat=OSPF&prio=3&num=30")
-        fetch(url+"?startTime=2015-08-28T10:\n" +
-            "00:00Z&endTime=2015-08-28T11:00:00Z"+val)
+        fetch(mainUrl)
             .then(function(response) {
                 return response.json();
             })
             .then(items => this.setState({ rowData: items }));
-
-
-        Promise.all([
-            fetch(url+"?startTime=2015-08-28T10:\n" +
-                "00:00Z&endTime=2015-08-28T11:00:00Z"+val),
-            fetch(url2+"?startTime=2015-08-28T10:\n" +
-                "00:00Z&endTime=2015-08-28T11:00:00Z"+val),
-            fetch(url3+"?startTime=2015-08-28T10:\n" +
-                "00:00Z&endTime=2015-08-28T11:00:00Z"+val)
-        ]).then(allResponses => {
-            const response1 = allResponses[0]
-            const response2 = allResponses[1]
-            const response3 = allResponses[2]
-
-
-        //    this.setState({ rowData: allArr })
-
-
-        })
+        console.log('first'+this.state.rowData)
+        //let mainUrl2=url2+"?startTime="+n+"&endTime="+endtime+val;
+        fetch(mainUrl)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(items => this.setState({ rowData: items }));
+        console.log('second'+this.state.rowData)
+       // let mainUrl3=url3+"?startTime="+n+"&endTime="+endtime+val;
+        fetch(mainUrl)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(items => this.setState({ rowData: items }));
+        console.log('third'+this.state.rowData)
     }
 
 
@@ -86,7 +116,15 @@ class App extends Component {
             <div className='wrapper'>
                 <div>
                     <div className='parent'>
-                        <div className='childItems'>Log Sources</div>
+                        <div className='childItems'>
+                            <select ref="logserver">
+                                <option value="">Log Sources</option>
+                                <option value="4001">Server 4001</option>
+                                <option value="4002">Server 4002</option>
+                                <option value="4003">Server 4003</option>
+                            </select>
+                        </div>
+
                         <select ref="pagezination">
                             <option value="">Entries Per Page</option>
                             <option value="50">50</option>
@@ -96,30 +134,29 @@ class App extends Component {
                     </div>
                     <div className='parent'>
                         <div className='childItems'>
-                            <select ref="starttime">
-                                <option>Start Date</option>
-                                {newdata.map(function(log, index) {
-                                    return <option  key={index} value={log.time}>{log.time}</option>
-                                })}
-                            </select>
+
+                            <DateTimePicker
+                                onChange={this.onChange}
+                                value={this.state.date}
+                            />
                         </div>
                         <div className='childItems'>to
-                            <select ref="endtime">
-                                <option>end</option>
-                                {newdata.map(function(log, index) {
-                                    return <option  key={index} value={log.time}>{log.time}</option>
-                                })}
-                            </select></div>
+
+                            <DateTimePicker
+                                onChange={this.onChangeEd}
+                                value={this.state.endDatePic}
+                            />
+                        </div>
                         <div className='childItems'>
                             <select ref="severity">
-                                <option>Log Severity</option>
+                                <option value="ser">Log Severity</option>
                                 {newdata.map(function(log, index) {
                                     return <option  key={index} value={log.prio}>{log.prio}</option>
                                 })}
                             </select></div>
                         <div className='childItems'>
                             <select ref="cat">
-                                <option>Log Category</option>
+                                <option value="clog">Log Category</option>
                                 {newdata.map(function(log, index) {
                                     return <option  key={index} value={log.cat}>{log.cat}</option>
                                 })}
