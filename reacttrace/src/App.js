b1b3eeb1ee740d4dbd072f5bbdf31b1114315e2d
mdkannan <mdkannandurai88@gmail.com>
2019-11-24 06:06:26 +0530

    second commit

--- reacttrace/src/App.js
+++ reacttrace/src/App.js
@@ -1,69 +1,175 @@
 import React from 'react';
 import { Component } from "react";
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 import './App.css';
 
 //function App() {
-    class App extends Component {
-        constructor(props) {
-            super(props);
-            this.state = { data: [] };
-            this.onSort = this.onSort.bind(this)
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            columnDefs: [
+                {headerName: "Date", field: "time"},
+                {headerName: "IP", field: "ip"},
+                {headerName: "Severity", field: "prio"},
+                {headerName: "Category", field: "cat"},
+                {headerName: "Event", field: "event"},
+                {headerName: "Action", field: "action"},
+                {headerName: "Message", field: "message"},
+            ],
+            paginationPageSize:50,
+            rowData: [],
+            allLog:[]
         }
 
-        componentDidMount() {
-            fetch("http://localhost:4002/log")
-                .then(function(response) {
-                    return response.json();
-                })
-                .then(items => this.setState({ data: items }));
-        }
+        this.onFind = this.onFind.bind(this)
+    }
 
-        onSort(event, sortKey){
-            /*
-            assuming your data is something like
-            [
-              {accountname:'foo', negotiatedcontractvalue:'bar'},
-              {accountname:'monkey', negotiatedcontractvalue:'spank'},
-              {accountname:'chicken', negotiatedcontractvalue:'dance'},
-            ]
-            */
-            const data = this.state.data;
-            data.sort((a ,b) => a[sortKey].localeCompare(b[sortKey]))
-            this.setState({data})
-        }
+    onFind(){
+        console.log(this.refs.endtime.value);
+        this.setState({paginationPageSie:this.refs.pagezination.value});
+        //http://localhost:12345/log?startTime=2015-08-28T10:
+        // 00:00Z&endTime=2015-08-28T11:00:00Z&cat=OSPF&prio=3&num=40
+        let url="http://localhost:4001/log";
+        let url2 ="http://localhost:4002/log";
+        let url3 ="http://localhost:4003/log";
+        let val ="&cat="+this.refs.cat.value+"&prio="+this.refs.severity.value+"&num=20";
+        //2015-08-28T10:59:53Z
+        let fullUrl=url+val;
+       // fetch(url+"?startTime=2015-08-28T10:\n" +
+       //     "00:00Z&endTime=2015-08-28T11:00:00Z&cat=OSPF&prio=3&num=30")
+        fetch(url+"?startTime=2015-08-28T10:\n" +
+            "00:00Z&endTime=2015-08-28T11:00:00Z"+val)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(items => this.setState({ rowData: items }));
 
-        render() {
-            var newdata = this.state.data;
-            console.log(newdata);
-            return ( <table className="m-table">
-                <thead>
-                <tr>
-                    <th  onClick={e => this.onSort(e, 'time')}>Date</th>
-                    <th  onClick={e => this.onSort(e, 'ip')} >IP</th>
-                    <th  onClick={e => this.onSort(e, 'prio')}>Severity</th>
-                    <th  onClick={e => this.onSort(e, 'cat')}>Category</th>
-                    <th  onClick={e => this.onSort(e, 'event')}>Event</th>
-                    <th  onClick={e => this.onSort(e, 'action')}>Action</th>
-                    <th >Message</th>
-                </tr>
-                </thead>
-                <tbody>
-                {newdata.map(function(log, index) {
-                    return (
-                        <tr key={index} data-item={log}>
-                            <td data-title="time">{log.time}</td>
-                            <td data-title="ip">{log.ip}</td>
-                            <td data-title="priority">{log.prio}</td>
-                            <td data-title="category">{log.cat}</td>
-                            <td data-title="event">{log.event}</td>
-                            <td data-title="action">{log.action}</td>
-                            <td data-title="message">{log.message}</td>
-                        </tr>
-                    );
-                })}
-                </tbody>
-            </table>);
-        }
+
+        Promise.all([
+            fetch(url+"?startTime=2015-08-28T10:\n" +
+                "00:00Z&endTime=2015-08-28T11:00:00Z"+val),
+            fetch(url2+"?startTime=2015-08-28T10:\n" +
+                "00:00Z&endTime=2015-08-28T11:00:00Z"+val),
+            fetch(url3+"?startTime=2015-08-28T10:\n" +
+                "00:00Z&endTime=2015-08-28T11:00:00Z"+val)
+        ]).then(allResponses => {
+            const response1 = allResponses[0]
+            const response2 = allResponses[1]
+            const response3 = allResponses[2]
+
+
+        //    this.setState({ rowData: allArr })
+
+
+        })
+    }
+
+
+    componentDidMount() {
+
+        fetch("http://localhost:4003/log")
+            .then(function(response) {
+                return response.json();
+            })
+            .then(items => this.setState({ rowData: items }));
+    }
+
+    render() {
+        let newdata = this.state.rowData;
+        console.log(newdata)
+        return (
+            <div className='wrapper'>
+                <div>
+                    <div className='parent'>
+                        <div className='childItems'>Log Sources</div>
+                        <select ref="pagezination">
+                            <option value="">Entries Per Page</option>
+                            <option value="50">50</option>
+                            <option value="30">30</option>
+                            <option value="100">100</option>
+                        </select>
+                    </div>
+                    <div className='parent'>
+                        <div className='childItems'>
+                            <select ref="starttime">
+                                <option>Start Date</option>
+                                {newdata.map(function(log, index) {
+                                    return <option  key={index} value={log.time}>{log.time}</option>
+                                })}
+                            </select>
+                        </div>
+                        <div className='childItems'>to
+                            <select ref="endtime">
+                                <option>end</option>
+                                {newdata.map(function(log, index) {
+                                    return <option  key={index} value={log.time}>{log.time}</option>
+                                })}
+                            </select></div>
+                        <div className='childItems'>
+                            <select ref="severity">
+                                <option>Log Severity</option>
+                                {newdata.map(function(log, index) {
+                                    return <option  key={index} value={log.prio}>{log.prio}</option>
+                                })}
+                            </select></div>
+                        <div className='childItems'>
+                            <select ref="cat">
+                                <option>Log Category</option>
+                                {newdata.map(function(log, index) {
+                                    return <option  key={index} value={log.cat}>{log.cat}</option>
+                                })}
+                            </select></div>
+                        <div className='childItems'>
+                            <select ref="eventlog">
+                                <option>log event</option>
+                                {newdata.map(function(log, index) {
+                                    return <option  key={index} value={log.event}>{log.event}</option>
+                                })}
+                            </select>
+                        </div>
+                        <div className='childItems'>
+                            <select ref="actionlog">
+                                <option>log action</option>
+                                {newdata.map(function(log, index) {
+                                    return <option  key={index} value={log.action}>{log.action}</option>
+                                })}
+                            </select>
+                        </div>
+                        <div className='childItems'>
+                            <select ref="ip">
+                                <option>LogIp</option>
+                                {newdata.map(function(log, index) {
+                                    return <option  key={index} value={log.ip}>{log.ip}</option>
+                                })}
+                            </select></div>
+                        <div className='childItems'>
+                            <button onClick={e => this.onFind(e, 'time')}>
+                            find
+                        </button>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    className="ag-theme-balham"
+                    style={{ height: '600px', width: '100%' }}
+                >
+                    <AgGridReact
+                        enableSorting={true}
+                        enableFilter={true}
+                        pagination={true}
+                        columnDefs={this.state.columnDefs}
+                    paginationPageSize={this.state.paginationPageSize}
+                        rowData={this.state.rowData}>
+                    </AgGridReact>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
